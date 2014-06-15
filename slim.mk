548554cf52d1c8c4a6abbbd5e9076bc93c331843
Bryne <bryne@blaquesoul.com>
2014-06-15 23:25:37 +0200

    Fixed slim.mk Signed-off-by: Bryne <bryne@blaquesoul.com> Change-Id: If7ad1952de2a4d1bc59b50c6a5b307f4c1cad9a4

---
+++ slim.mk
@@ -0,0 +1,27 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# Inherit some common SLIM stuff
+$(call inherit-product, vendor/slim/config/gsm.mk)
+
+# Inherit some common SLIM stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/i9500/full_i9500.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_NAME := slim_i9500
+PRODUCT_DEVICE := i9500
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_MODEL=GT-I9500 \
+    PRODUCT_NAME=ja3gxx \
+    PRODUCT_DEVICE=ja3g \
+    TARGET_DEVICE=ja3g \
+    BUILD_FINGERPRINT="samsung/ja3gxx/ja3g:4.4.2/KOT49H/I9500XXUFNB3:user/release-keys" \
+    PRIVATE_BUILD_DESC="ja3gxx-user 4.4.2 KOT49H I9500XXUFNB3 release-keys"
