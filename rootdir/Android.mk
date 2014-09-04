8ec262ee74a0e5591b8027b8b447707199cf084a
Alberto96 <albertoguanti@gmail.com>
2014-09-04 16:58:01 +0200

    rootdir: fix Android.mk for pulsing LED while boot

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -37,6 +37,7 @@ LOCAL_MODULE		:= init.led.sh
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/init.led.sh
+LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
