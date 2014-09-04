696d7452358db1e03309f30f411ebf56e36e6fe0
Alberto96 <albertoguanti@gmail.com>
2014-09-04 19:46:45 +0200

    rootdir: init.led.sh: force disable pulsing LED when boot completed
    `cat` seems to give just an empty value so let's force disable it

--- rootdir/etc/init.led.sh
+++ rootdir/etc/init.led.sh
@@ -1,6 +1,4 @@
 #!/system/bin/sh
 
-# prevent boot led from overriding charging led
-if [ `cat /sys/class/sec/led/led_pattern` = "6" ]; then
-	echo 0 > /sys/class/sec/led/led_pattern
-fi
+echo "Force Disable Pulsing LED..."
+echo 0 > /sys/class/sec/led/led_pattern
