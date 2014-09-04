0b20798929da721c2bc15e99f98616d0021ee835
Alberto96 <albertoguanti@gmail.com>
2014-09-04 17:19:38 +0200

    rootdir: ramdisk cleanup
    another of another of another ...

--- rootdir/etc/init.led.sh
+++ rootdir/etc/init.led.sh
@@ -2,5 +2,5 @@
 
 # prevent boot led from overriding charging led
 if [ `cat /sys/class/sec/led/led_pattern` = "6" ]; then
-    echo 0 > /sys/class/sec/led/led_pattern
+	echo 0 > /sys/class/sec/led/led_pattern
 fi
