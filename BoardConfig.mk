a63b80eefa6b27f77e6d77497792796c2fc2f5c6
Alberto96 <albertoguanti@gmail.com>
2014-12-31 18:21:06 +0100

    i9500: ril: remove custom ril class
    test

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -127,7 +127,6 @@ BOARD_NFC_HAL_SUFFIX := universal5410
 # Radio
 BOARD_PROVIDES_LIBRIL := true
 BOARD_MODEM_TYPE := xmm6360
-BOARD_RIL_CLASS := ../../../device/samsung/i9500/ril/
 
 # Wifi
 BOARD_HAVE_SAMSUNG_WIFI          := true
