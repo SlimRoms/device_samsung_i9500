e72ed1e705662c14a6b850ea1f6f74252bbd412f
Alberto96 <albertoguanti@gmail.com>
2015-01-31 23:12:02 +0100

    i9500: updates from I9500XXUHOA7

--- configs/phone-xxhdpi-2048-dalvik-heap.mk
+++ configs/phone-xxhdpi-2048-dalvik-heap.mk
@@ -18,9 +18,9 @@
 # 192m of RAM gives enough space for 5 8 megapixel camera bitmaps in RAM.
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapstartsize=16m \
+    dalvik.vm.heapstartsize=8m \
     dalvik.vm.heapgrowthlimit=256m \
     dalvik.vm.heapsize=512m \
     dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=512k \
+    dalvik.vm.heapminfree=2m \
     dalvik.vm.heapmaxfree=8m
