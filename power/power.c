c477cbc45687ed8bc8fca15d25ebb255023ae0c8
Alberto96 <albertoguanti@gmail.com>
2015-03-31 17:59:01 +0200

    i9500: power: Find the right inputs by name
    Rather than hard-coding the inputs (which change if the kernel
    changes), look at the name sysfs entry to identify the right
    paths.
    
    This requires that init correctly locate and set permissions
    on these paths as well.
    
    by @crpalmer

--- power/power.c
+++ power/power.c
@@ -18,6 +18,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <ctype.h>
 
 #define LOG_TAG "JA3G PowerHAL"
 #include <utils/Log.h>
@@ -25,16 +26,53 @@
 #include <hardware/hardware.h>
 #include <hardware/power.h>
 
-#define TSP_POWER "/sys/class/input/input1/enabled"
-#define GPIO_KEYS_POWER "/sys/class/input/input14/enabled"
-#define TOUCHKEY_POWER "/sys/class/input/input15/enabled"
+#define MAX_INPUTS 20
+#define INPUT_PREFIX "/sys/class/input/input"
+#define MAX_PATH_SIZE (strlen(INPUT_PREFIX) + 20)
 
-static void sysfs_write(char *path, char *s) {
+static const char *names[] = { "sec_touchscreen", "sec_touchkey", "gpio-keys" };
+#define N_NAMES (sizeof(names) / sizeof(names[0]))
+static char *paths[N_NAMES];
+static int have_found_paths;
+
+static size_t sysfs_read(char *path, char *buffer, size_t n)
+{
+    char buf[80];
+    int fd;
+    ssize_t len;
+
+    if ((fd = open(path, O_RDONLY)) < 0) {
+        if (errno != ENOENT) {
+            strerror_r(errno, buf, sizeof(buf));
+            ALOGE("Error opening %s: %s\n", path, buf);
+        }
+        return 0;
+    }
+
+    len = read(fd, buffer, n);
+    if (len < 0) {
+        strerror_r(errno, buf, sizeof(buf));
+        ALOGE("Error reading from %s: %s\n", path, buf);
+        return 0;
+    }
+
+    while (len > 0 && isspace(buffer[len-1])) len--;
+    if ((size_t) len < n) buffer[len] = '\0';
+
+    close(fd);
+
+    return len;
+}
+
+static void sysfs_write(char *path, char *s)
+{
     char buf[80];
     int len;
-    int fd = open(path, O_WRONLY);
+    int fd;
 
-    if (fd < 0) {
+    if (path == NULL) return;
+
+    if ((fd = open(path, O_WRONLY)) < 0) {
         strerror_r(errno, buf, sizeof(buf));
         ALOGE("Error opening %s: %s\n", path, buf);
         return;
@@ -49,16 +87,42 @@ static void sysfs_write(char *path, char *s) {
     close(fd);
 }
 
+static void find_paths(void)
+{
+    size_t i, j;
+    char path[MAX_PATH_SIZE];
+    char name[20];
+
+    for (i = 0; i < MAX_INPUTS; i++) {
+        sprintf(path, "%s%d/name", INPUT_PREFIX, i);
+        if (sysfs_read(path, name, sizeof(name)) > 0) {
+            for (j = 0; j < N_NAMES; j++) {
+                if (strcmp(name, names[j]) == 0) {
+                    paths[j] = malloc(MAX_PATH_SIZE);
+                    sprintf(paths[j], "%s%d/enabled", INPUT_PREFIX, i);
+                    ALOGD("%s => %s\n", names[j], paths[j]);
+                }
+            }
+        }
+    }
+}
+
 static void power_init(struct power_module *module)
 {
 }
 
 static void power_set_interactive(struct power_module *module, int on)
 {
+    size_t i;
+
     ALOGD("%s: %s input devices", __func__, on ? "enabling" : "disabling");
-    sysfs_write(TSP_POWER, on ? "1" : "0");
-    sysfs_write(TOUCHKEY_POWER, on ? "1" : "0");
-    sysfs_write(GPIO_KEYS_POWER, on ? "1" : "0");
+
+    if (! have_found_paths) {
+        find_paths();
+        have_found_paths = 1;
+    }
+
+    for (i = 0; i < N_NAMES; i++) sysfs_write(paths[i], on ? "1" : "0");
 }
 
 static void power_hint(struct power_module *module, power_hint_t hint,
