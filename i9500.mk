2cee0a4d270777ac97e27e99bce8c757930870a2
Ethan Chen <intervigil@gmail.com>
2015-05-10 15:22:26 -0700

    i9500: Sort system properties
    Conflicts:
    	i9500.mk
    	system.prop
    
    Change-Id: Icae3ad14d88e25392e43572aee78e4b295b655b9

--- i9500.mk
+++ i9500.mk
@@ -239,6 +239,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	dalvik.vm.isa.arm.features=lpae,div
 
 # Hardware Permissions
+=======
+# Permissions
 PRODUCT_COPY_FILES += \
 	external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml \
 	frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
