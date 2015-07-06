9809a152c47aa13d1cca7459eea1493b5c2f8a96
Alberto96 <albertoguanti@gmail.com>
2015-07-06 15:52:03 +0200

    i9500: rootdir: clean-up

--- rootdir/etc/synapse.sh
+++ rootdir/etc/synapse.sh
@@ -3,13 +3,15 @@
 mount -t rootfs -o remount,rw rootfs
 
 chmod 777 -R /res/synapse
+ln -s /res/synapse/uci /sbin/uci
+/sbin/uci
 
 for i in /sys/block/*/queue/add_random;do echo 0 > $i;done
-
-ln -s /res/synapse/uci /sbin/uci
-
-/sbin/uci
 
 mkdir -p /mnt/ntfs
 chmod 777 /mnt/ntfs
 mount -o mode=0777,gid=1000 -t tmpfs tmpfs /mnt/ntfs
+
+mount -w -o remount /system
+rm -f /system/bin/auditd
+mount -r -o remount /system
