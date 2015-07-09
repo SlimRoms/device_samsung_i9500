f51b93006d30e5c6e0ee81023b65d6a98a4f7b7d
Alberto96 <albertoguanti@gmail.com>
2015-07-09 15:12:01 +0200

    i9500: restore cbd to the system binary dir
    reduce boot/recovery image size

--- i9500.mk
+++ i9500.mk
@@ -166,8 +166,8 @@ PRODUCT_PACKAGES += \
 	OTAUpdates
 
 # Radio
-PRODUCT_PACKAGES += \
-	cbd
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/ril/sbin/cbd:system/bin/cbd
 
 PRODUCT_PACKAGES += \
 	libsecril-client \
