76ff2d55b1caa89a10c6ab0b05ccbc60acd09180
Nir Bruderman <nbruderman@gmail.com>
2015-07-13 10:41:08 +0300

    i9500: SELinux policy for L
    All the credit for this goes to @intervigilium
    He wrote the whole patch

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -219,28 +219,24 @@ BACKLIGHT_PATH := "/sys/class/backlight/panel/brightness"
 CHARGING_ENABLED_PATH := "/sys/class/power_supply/battery/batt_lp_charging"
 
 # SELinux
-#BOARD_SEPOLICY_DIRS := \
-#	device/samsung/i9500/sepolicy
-#
-#BOARD_SEPOLICY_UNION := \
-#	app.te \
-#	cpboot-daemon.te \
-#	domain.te \
-#	exyrngd.te \
-#	file.te \
-#	file_contexts \
-#	gpsd.te \
-#	kernel.te \
-#	lmkd.te \
-#	init.te \
-#	macloader.te \
-#	recovery.te \
-#	system_server.te \
-#	rild.te \
-#	ueventd.te \
-#	uncrypt.te \
-#	vold.te \
-#	wpa.te
+BOARD_SEPOLICY_DIRS += \
+    device/samsung/i9500/sepolicy
+
+BOARD_SEPOLICY_UNION := \
+    device.te \
+    drmserver.te \
+    file_contexts \
+    file.te \
+    gpsd.te \
+    mediaserver.te \
+    property_contexts \
+    property.te \
+    pvrsrvctl.te \
+    rild.te \
+    shell.te \
+    system_server.te \
+    ueventd.te \
+    wpa.te
 
 # Releasetools
 #TARGET_RELEASETOOLS_EXTENSIONS := $(LOCAL_PATH)
