c02f3bb30616d5c8935a8d3e16e692ce3bec2305
Alberto96 <albertoguanti@gmail.com>
2015-07-14 18:18:35 +0200

    i9500: bluetooth: increase bluetooth stability & performance
    used stock libbt-vendor.so as example (reverse-engineered)
    
    test

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -17,6 +17,9 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "GT-I9500"
+#define BTM_DEF_LOCAL_NAME   "Samsung Galaxy S IV"
+
+#define BTA_DISABLE_DELAY 1000 /* in milliseconds */
+#define BTM_BLE_ADV_TX_POWER {-21, -15, -7, 1, 9}
 
 #endif
