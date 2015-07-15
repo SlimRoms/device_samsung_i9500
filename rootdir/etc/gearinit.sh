3c6bf965a30f7e97d4ccb1625bae17b5122acb43
Nir Bruderman <nbruderman@gmail.com>
2015-07-16 00:42:30 +0300

    i9500: Move the equalizer settings forward
    Maybe causing problem of setting it

--- rootdir/etc/gearinit.sh
+++ rootdir/etc/gearinit.sh
@@ -9,14 +9,6 @@ ln -s /res/synapse/uci /sbin/uci
 
 for i in /sys/block/*/queue/add_random;do echo 0 > $i;done
 
-echo "0x0FA4 0x0404 0x0170 0x1DB9 0xF233 0x040B 0x08B6 0x1977 0xF45E 0x040A 0x114C 0x0B43 0xF7FA 0x040A 0x1F97 0xF41A 0x0400 0x1068" > /sys/class/misc/wolfson_control/eq_sp_freqs
-echo -5 > /sys/class/misc/wolfson_control/eq_sp_gain_1
-echo 1 > /sys/class/misc/wolfson_control/eq_sp_gain_2
-echo 0 > /sys/class/misc/wolfson_control/eq_sp_gain_3
-echo 4 > /sys/class/misc/wolfson_control/eq_sp_gain_4
-echo 3 > /sys/class/misc/wolfson_control/eq_sp_gain_5
-echo 1 > /sys/class/misc/wolfson_control/switch_eq_speaker
-
 mkdir -p /mnt/ntfs
 chmod 777 /mnt/ntfs
 mount -o mode=0777,gid=1000 -t tmpfs tmpfs /mnt/ntfs
@@ -38,6 +30,14 @@ cp -p "/system/xbin/su" "/system/bin/.ext/.su"
 
 rm -f "/system/bin/app_process"
 ln -s "/system/xbin/daemonsu" "/system/bin/app_process"
+
+echo "0x0FA4 0x0404 0x0170 0x1DB9 0xF233 0x040B 0x08B6 0x1977 0xF45E 0x040A 0x114C 0x0B43 0xF7FA 0x040A 0x1F97 0xF41A 0x0400 0x1068" > /sys/class/misc/wolfson_control/eq_sp_freqs
+echo -5 > /sys/class/misc/wolfson_control/eq_sp_gain_1
+echo 1 > /sys/class/misc/wolfson_control/eq_sp_gain_2
+echo 0 > /sys/class/misc/wolfson_control/eq_sp_gain_3
+echo 4 > /sys/class/misc/wolfson_control/eq_sp_gain_4
+echo 3 > /sys/class/misc/wolfson_control/eq_sp_gain_5
+echo 1 > /sys/class/misc/wolfson_control/switch_eq_speaker
 
 for BIT in "64" "32"; do
 	if [[ -f "/system/bin/app_process${BIT}" ]]; then
