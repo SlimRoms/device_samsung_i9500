e39ad4d897e87f839ad115914e90945e4265727e
Alberto96 <albertoguanti@gmail.com>
2015-07-18 20:40:08 +0200

    i9500: rootdir: gearinit.sh: clean-up & optimizations

--- rootdir/etc/gearinit.sh
+++ rootdir/etc/gearinit.sh
@@ -1,12 +1,15 @@
 #!/sbin/busybox sh
 
+# Mount / as RW
 mount -t rootfs -o remount,rw rootfs
 
+# Initialize Synapse
 chown -R root.root /res/synapse
 chmod -R 777 /res/synapse
 ln -s /res/synapse/uci /sbin/uci
 /sbin/uci
 
+# Initialize Wolfson Sound Control
 echo "0x0FA4 0x0404 0x0170 0x1DB9 0xF233 0x040B 0x08B6 0x1977 0xF45E 0x040A 0x114C 0x0B43 0xF7FA 0x040A 0x1F97 0xF41A 0x0400 0x1068" > /sys/class/misc/wolfson_control/eq_sp_freqs
 echo -5 > /sys/class/misc/wolfson_control/eq_sp_gain_1
 echo 1 > /sys/class/misc/wolfson_control/eq_sp_gain_2
@@ -19,15 +22,23 @@ echo 1 > /sys/class/misc/wolfson_control/switch_eq_speaker
 echo 0 > /sys/class/misc/wolfson_control/switch_eq_speaker
 echo 1 > /sys/class/misc/wolfson_control/switch_eq_speaker
 
+# PVR GPU Tweaks
+echo 0 > /sys/module/pvrsrvkm/parameters/gPVRDebugLevel
+echo 0 > /sys/module/pvrsrvkm/parameters/gPVREnableVSync
+
+# I/O Tweaks
 for i in /sys/block/*/queue/add_random;do echo 0 > $i;done
 
+# Initialize NTFS Driver
 mkdir -p /mnt/ntfs
 chmod 777 /mnt/ntfs
 mount -o mode=0777,gid=1000 -t tmpfs tmpfs /mnt/ntfs
 
+# Remove unused auditd binary
 mount -w -o remount /system
 rm -f /system/bin/auditd
 
+# Initialize SuperSU
 set -e
 
 if [[ -f "/system/bin/mksh" ]]; then
