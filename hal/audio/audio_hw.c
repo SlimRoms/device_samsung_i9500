be1b6b83ac7f0bc6acc0621e93694df5e7ac9e31
Alberto96 <albertoguanti@gmail.com>
2015-07-29 17:17:20 +0200

    i9500: audio: restore "__used" property to flags variable

--- hal/audio/audio_hw.c
+++ hal/audio/audio_hw.c
@@ -1909,7 +1909,7 @@ static int adev_open_input_stream(struct audio_hw_device *dev,
                                   audio_devices_t devices,
                                   struct audio_config *config,
                                   struct audio_stream_in **stream_in,
-                                  audio_input_flags_t flags,
+                                  audio_input_flags_t flags __unused,
                                   const char *address __unused,
                                   audio_source_t source __unused)
 {
