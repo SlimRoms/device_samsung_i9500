1cd103015ee0ab9d04086e1c14aed25deaba25ee
Alberto96 <albertoguanti@gmail.com>
2015-07-31 01:17:14 +0200

    i9500: THIS DUMB WORKAROUND WAS CAUSING AUDIO TO DISAPPEAR
    jfhdskhlghdhgdhgshlhgdhlgkjh

--- i9500.mk
+++ i9500.mk
@@ -44,8 +44,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf \
 	$(LOCAL_PATH)/configs/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
-	$(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
-	$(LOCAL_PATH)/configs/audio/silence.wav:system/etc/sound/silence.wav
+	$(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
 PRODUCT_PACKAGES += \
 	audio.a2dp.default \
