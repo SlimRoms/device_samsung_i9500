03f43d0d12ae0ec2bfd353b3b46b7538b37a2d97
Alberto96 <albertoguanti@gmail.com>
2015-09-20 11:16:03 +0200

    Revert "i9500: audio: switch to Narrowband AMR for BT SCO"
    * If you like funny sound, then revert this reverted commit ;)

--- hal/audio/audio_hw.c
+++ hal/audio/audio_hw.c
@@ -560,10 +560,16 @@ static void start_bt_sco(struct audio_device *adev)
     ALOGV("%s: Opening SCO PCMs", __func__);
 
     bool use_dyn_wb_amr = property_get_bool("persist.call.dynamic.wb_amr", false);
+    bool using_chn_modem = property_get_bool("persist.device.uses.chn_modem", false);
 
     if (!use_dyn_wb_amr) {
-        sco_config = &pcm_config_sco;
-        ALOGV("%s: Forced In-Call Narrowband AMR", __func__);
+        if (!using_chn_modem) {
+            sco_config = &pcm_config_sco_wide;
+            ALOGV("%s: Forced In-Call Wideband AMR", __func__);
+        } else {
+            sco_config = &pcm_config_sco;
+            ALOGV("%s: Forced In-Call Narrowband AMR", __func__);
+        }
     } else {
         ALOGV("%s: Using Dynamic Wideband AMR", __func__);
         if (adev->wb_amr) {
