94da8bc9995d1cc9de4dcde833e1537b366106bc
Nir Bruderman <nbruderman@gmail.com>
2015-10-08 04:15:25 +0300

    i9500: remove BOARD_SEPOLICY_UNION
    all files found in BOARD_SEPOLICY_DIRS are now implicitly unioned

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -145,23 +145,6 @@ TARGET_USERIMAGES_USE_F2FS := true
 BOARD_SEPOLICY_DIRS += \
     device/samsung/i9500/sepolicy
 
-BOARD_SEPOLICY_UNION := \
-    bluetooth.te \
-    device.te \
-    drmserver.te \
-    file_contexts \
-    file.te \
-    gpsd.te \
-    mediaserver.te \
-    property_contexts \
-    property.te \
-    pvrsrvctl.te \
-    rild.te \
-    shell.te \
-    system_server.te \
-    ueventd.te \
-    wpa.te
-
 # TWRP Specific
 BOARD_HAS_NO_REAL_SDCARD := true
 RECOVERY_SDCARD_ON_DATA := true
