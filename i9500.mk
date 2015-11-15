b31c24fcfb39480ddc7feec0fab2166ebd2025ee
Alberto96 <albertoguanti@gmail.com>
2015-11-15 20:57:57 +0100

    i9500: compile libhwjpeg from sources
    Change-Id: I314fff682f3c24f5a1f464824d1cf4889838fda7

--- i9500.mk
+++ i9500.mk
@@ -87,6 +87,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     camera2.portability.force_api=1
 
+PRODUCT_PACKAGES += \
+    libhwjpeg
+
 # Charger
 PRODUCT_PACKAGES += \
     charger_res_images
